fbd31cb330b1cbbbe090a10a4d565383c5d6c3d2
Tyuiu.VlasenkoDA@outlook.com <Tyuiu.VlasenkoDA@outlook.com>
2024-09-16 13:53:28 +0500

    Task1

---
+++ Tyuiu.VlasenkoDA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tyuiu.VlasenkoDA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Игорь";
+            int age = 45;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey(); // позволяет дождаться нажатия клавиши, чтобы окно консоли не закрылось сразу
+        }
+    }
+}
