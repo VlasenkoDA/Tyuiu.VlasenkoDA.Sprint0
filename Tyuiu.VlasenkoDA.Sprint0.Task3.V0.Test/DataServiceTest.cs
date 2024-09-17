09c63bbcdd296fdc0f0a5596052f1d4045f79564
Tyuiu.VlasenkoDA@outlook.com <Tyuiu.VlasenkoDA@outlook.com>
2024-09-17 20:13:27 +0500

    Task3

---
+++ Tyuiu.VlasenkoDA.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+using Tyuiu.VlasenkoDA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.VlasenkoDA.Sprint0.Task3.V0.Test
+
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.VlasenkoDA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Tyuiu.VlasenkoDA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.VlasenkoDA.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+            
+        }
+    }
+}
