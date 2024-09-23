186226944ab23ea7f4dd4afc703da9c80979a8e8
Tyuiu.VlasenkoDA@outlook.com <Tyuiu.VlasenkoDA@outlook.com>
2024-09-23 12:45:36 +0500

    Task4

---
+++ Tyuiu.VlasenkoDA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Tyuiu.VlasenkoDA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.VlasenkoDA.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Subtraction(15, 5));
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            Console.WriteLine(DataService.Division(5, 5));
+            Console.ReadKey();
+        }
+    }
+}
+
+
